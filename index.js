79c77165c69dacecc19b7e45adc0edcba7fa8576
Nicke <nicholas0012@gmail.com>
2019-09-16 21:33:14 +0200

    Comments

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const log = require("./log");
+
+//Initialization
 const app = express();
 
+//Universal Middleware
 app.use(express.urlencoded({extended: false}));
 app.use(log);
+
+//Routes
 app.get('/home', (req, res)=>{
     res.send("ganggang");
 });
 
+//Porting
 app.listen(5200, ()=>console.log("Running on port 5200"));
